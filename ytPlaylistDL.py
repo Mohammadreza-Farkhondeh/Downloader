1695d5ca3ca77f7c0a416073d4d932c3fe829c4c
Mohammadreza Farkhonde <mohammr.farkhonde12@sharif.edu>
2023-06-04 22:25:09 +0330

    commited

--- ytPlaylistDL.py
+++ ytPlaylistDL.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.action_chains import ActionChains
 
 URL = input('playlist page URL: ')
-urlToDL = URL.split('.com')
-urlToDL = urlToDL[0]+'pp.com'+urlToDL[1]
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get(URL)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
@@ -14,27 +14,48 @@ all_links = soup.find_all('a', {'class': "yt-simple-endpoint style-scope ytd-pla
 Quality = input('Quality (1080, 720, 480, 360, 240, 144): ')
 
 
-def wait_to_convert_then_return_href(s):
-    div = s.find('div', {'id': "process-result"})
-    if div.text == "":
+def find_quality(s):
+    trs = s.find_all('tr')
+    for tr in trs:
+        td = tr.find(td)
+        if td.text == f'(.mp4) {Quality}p':
+            return tr
+
+
+def wait_to_convert_then_return_href(tr):
+    td = tr.find_all('td')[2]
+    btn = td.find('button')
+    a = btn.find('a')
+    if a.text != "Download":
         time.wait(1)
         wait_to_convert_then_return_href(s)
-    elif div.text == " Download .mp4":
-        href = div.find('a')['href']
+    else:
+        href = a['href']
         return href
 
 
+def click_to_convert(tr):
+    td = tr.find_all('td')[2]
+    btn = td.find('button')
+    span = btn.find('span')
+    if span.text == "Convert":
+        btn.click()
+
+
 for i in range(len(all_links)):
-    all_links[i] = urlToDL+all_links[i]['href']
+    all_links[i] = 'https://www.youtubepi.com'+all_links[i]['href']
+
+print(all_links)
+
+for i in range(len(all_links)):
+    driver.switch_to.window(driver.window_handles[0])
     driver.get(all_links[i])
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    buttons = soup.find_all('button', {'class': 'btn btn-success'})
-
-    for btn in buttons:
-        if btn.text == Quality:
-            btn.click()
-
-    href = wait_to_convert_then_return_href(soup)
-    webdriver.Firefox().get(href)
+    tr = find_quality(s)
+    click_to_convert(tr)
+    href = wait_to_convert_then_return_href(tr)
+    driver.send_keys(Keys.CONTROL + 't')
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.get(href)
 
